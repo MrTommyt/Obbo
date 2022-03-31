65e4d4f17516c5aa12d624b863aae7b337c0a032
MrTommyt <benavidescamilo199@gmail.com>
2022-03-31 16:00:48 -0500

    Fix test

--- src/test/java/mr/tommy/obbo/test/methodinvoke/Wrapper.java
+++ src/test/java/mr/tommy/obbo/test/methodinvoke/Wrapper.java
@@ -2,7 +2,7 @@ package mr.tommy.obbo.test.methodinvoke;
 
 import mr.tommy.obbo.entity.Proxy;
 
-@Proxy("@base@.simpletest.@v@.@c@")
+@Proxy("@base@.methodinvoke.@v@.@c@")
 public interface Wrapper {
     int method();
 }
